ddbddbb155add1e696aae6a82b61372d8e605b02
sek5575 <eungyusong@gmail.com>
2016-05-29 21:59:09 +0900

    로그인까지 완료

--- login_proc.php
+++ login_proc.php
@@ -1,35 +1,34 @@
 <?php
-	session_start();
 	$connect = @mysql_connect('localhost', 'root', 'song');
 	$db_con = mysql_select_db("today", $connect);
 
 	$userid = $_GET['username'];
 	$userpw = $_GET['password'];
 
-	if($connect){
-  		echo "<script>alert('데이터베이스에 접속했습니다.');</script>";
-	}
-	if(empty($userid==true)
-	{
-		echo "<script>alert('아이디가 입력되지 않았습니다.');</script>";
-	}
-	if(empty($userpw)==true)
-	{
-		echo "<script>alert('비밀번호가 입력되지 않았습니다.');</script>";
-	}
+	if(empty($userid))
+   {
+      echo "<script>alert('아이디가 입력되지 않았습니다.');</script>";
+   }
+   else if(empty($userpw))
+   {
+      echo "<script>alert('비밀번호가 입력되지 않았습니다.');</script>";
+   }
+   else {
+      $sql = "select * from user where id='$userid' and password='$userpw'";
+      $result = mysql_query($sql);
 
-	$sql = "select * from from user where id='$userid' and password='$userpw'";
-	$result = mysql_query($sql);
+      $row = mysql_fetch_row($result);
+      $count = $row[0];
 
-	$count = mysql_num_rows($result);
-	if($count==1){
-		session_register("userid");
-		header("location: main.php");
-	}
-	else
-	{
-		echo "<script>alert('아이디/패스워드가 일치하지 않습니다.');</script>";
-	}
-
+      if(!isset($count)){
+         echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다.');history.back();</script>";
+      }
+      else{
+         session_start();
+         $_SESSION['userid'] = $userid;
+         $_SESSION['userpw'] = $userpw;
+            echo "<script> alert('로그인 성공!'); window.location.replace('main.php');</script>";
+      }
+  }
 
  ?>
---
+++ main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+<?
+session_start();
+?>
+<body>
+<h2>접속완료</h2>
+
+</body>
+
+</html>
