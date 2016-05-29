1bcbc96782fd51cfe4dd19cca073796762f746a6
sek5575 <eungyusong@gmail.com>
2016-05-29 23:40:53 +0900

    아이디 중복 체크

--- membersave.php
+++ membersave.php
@@ -6,11 +6,22 @@
 	$password=$_GET['pwd'];
 
 	$sql = "insert into user (id, password) values ('$id', '$password')";
-	if((isset($id) && isset($password))){
-		mysql_query($sql);
-		echo "<script>alert('입력되었습니다.');</script>";
+
+
+	$check = "select * from user where id='$id'";
+    $result = mysql_query($check);
+    $row = mysql_fetch_row($result);
+
+	if(strcmp($id, $row[0]) == 0){
+		echo "<script>alert('동일한 아이디가 존재합니다.');</script>";
 	}
 	else{
-		echo "<script>alert('오류!');</script>";
+		if((isset($id) && isset($password))){
+			mysql_query($sql);
+			echo "<script>alert('입력되었습니다.');window.location.replace('login.php');</script>";
+		}
+		else{
+			echo "<script>alert('오류!');</script>";
+		}
 	}
 ?>
