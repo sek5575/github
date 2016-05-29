4ca0243baf7c46dc244d57b6e387b62591fd8b52
sek5575 <eungyusong@gmail.com>
2016-05-29 20:37:36 +0900

    후....

--- login.php
+++ login.php
@@ -3,12 +3,11 @@
 
 <body>
 <h2>로그인</h2>
-  <form action='login_proc.php' method='GET'>
+	<form action='login_proc.php' method='GET'>
     <p>ID : <input type ='text' name='username' /></p>
     <p>PASSWORD : <input type = 'text' name='password' /></p>
     <input type ='submit' value='로그인'/>
-  </form>
-
-  </body>
+    </form>
+</body>
 
 </html>
--- login_proc.php
+++ login_proc.php
@@ -3,26 +3,33 @@
 	$connect = @mysql_connect('localhost', 'root', 'song');
 	$db_con = mysql_select_db("today", $connect);
 
+	$userid = $_GET['username'];
+	$userpw = $_GET['password'];
+
 	if($connect){
-  		echo "<script>alert('데이터베이스에 접속했습니다..');</script>";
+  		echo "<script>alert('데이터베이스에 접속했습니다.');</script>";
 	}
-
-	$userid = 'id';
-	$userpw = 'password';
-
-	$result = mysql_query("select id, password from user where id = $userid, password = $userpw");
-	$output = mysql_fetch_assoc($result);
-
-	if(empty($_GET['username'])==true)
+	if(empty($userid==true)
 	{
 		echo "<script>alert('아이디가 입력되지 않았습니다.');</script>";
 	}
-	if(empty($_GET['password'])==true)
+	if(empty($userpw)==true)
 	{
 		echo "<script>alert('비밀번호가 입력되지 않았습니다.');</script>";
 	}
 
-	
+	$sql = "select * from from user where id='$userid' and password='$userpw'";
+	$result = mysql_query($sql);
+
+	$count = mysql_num_rows($result);
+	if($count==1){
+		session_register("userid");
+		header("location: main.php");
+	}
+	else
+	{
+		echo "<script>alert('아이디/패스워드가 일치하지 않습니다.');</script>";
+	}
 
 
  ?>
