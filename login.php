a0a446a5cf599ac4547813ce7c8424ed07e418fd
sek5575 <eungyusong@gmail.com>
2016-06-02 00:37:32 +0900

    정규식 추가

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 	</style>
 </head>
 <body>
-<table border="1" style = "width: 1100px; height: 800px; margin: 0 auto">
+<table style = "width: 1100px; height: 800px; margin: 0 auto">
 	<tr>
 		<td style = "width: 800px; height: 100px">
 		<div style = "text-align: center">
@@ -15,13 +15,13 @@
 
 		<td style = "width: 300px; height: 100px">
 		<form action="login_proc.php" method="GET">
-		<table border="1" style = "margin: 0 auto">
+		<table style = "margin: 0 auto">
 			<tr>
 				<td>
 					<P>아이디</P>
 				</td>
 				<td>
-					<input type = 'text' name='username' />
+					<input type = 'text' name='username' pattern = ".{4,15}" title = "4~15자로 입력해주세요." />
 				</td>
 			</tr>
 			<tr>
@@ -29,7 +29,7 @@
 					<p>비밀번호</p>
 				</td>
 				<td>
-					<input type = 'password' name='password' />
+					<input type = 'password' name='password' pattern = ".{4,15}" title = "4~15자로 입력해주세요." />
 				</td>
 			</tr>
 			<tr>
@@ -42,46 +42,75 @@
 		</table>
 		</form>
 		</td>
-
 	</tr>
-
+	
 	<tr>
 		<td>
 		</td>
 
 		<td>
 		<form name="join" method="GET" action="membersave.php">
-		<table border="1" style = "margin: 0 auto">
+		<table style = "margin: 0 auto">
 			<tr>
 				<td>
-					<input type="text" name="newid" placeholder = "아이디" style = "width: 150px; height: 40px">
+					<input type="text" name="newid" placeholder = "아이디" style = "width: 150px; height: 40px"  pattern = ".{4,15}" title = "4~15자로 입력해주세요." >
 				</td>
 				<td>
-					<input type="password" name="newpwd" placeholder = "비밀번호" style = "width: 150px; height: 40px">
+					<input type="password" name="newpwd" placeholder = "비밀번호" style = "width: 150px; height: 40px" pattern = ".{4,15}" title = "4~15자로 입력해주세요." >
 				</td>
 			</tr>
 
 			<tr>
 				<td colspan=2>
-					<input type="text" name="mail" placeholder = "e-mail" style = "width: 300px; height: 40px">
+					<input type="text" name="mail" pattern = ".{1,15}@.{1,15}\..{1,15}" title = "e-mail 주소를 정확하게 넣어주세요." placeholder = "e-mail" style = "width: 300px; height: 40px">
 				</td>
 			</tr>
 
 			<tr>
 				<td colspan=2>
-					<input type="text" name="mail" placeholder = "e-mail" style = "width: 300px; height: 40px">
+					<input type="text" name="phonenum" pattern = "[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}" title = "번호를 정확하게 넣어주세요." placeholder = "000-0000-0000" style = "width: 300px; height: 40px ">
 				</td>
 			</tr>
 
 			<tr>
 				<td colspan=2>
-					<input type="text" name="phonenum" placeholder = "000-0000-0000" style = "width: 300px; height: 40px">
+					<script language = "javascript">
+					var sel1, sel2, sel3;
+					var today = new Date();
+					var i=0;
+
+					window.onload = function (){
+					sel1 = document.join.year;	
+					sel1.length = 0;
+					for (var i = 0; i < 116; i++)
+					sel1.add(new Option(1900+i+"년", 1900+i), i);
+
+					sel2 = document.join.month;
+					sel2.length = 0;
+					for (var i = 0; i < 12; i++)
+					sel2.add(new Option(1+i+"월", 1+i), i);
+			
+					sel3 = document.join.day;
+					sel3.length = 0;
+					for (var i = 0; i < 31; i++)
+					sel3.add(new Option(1+i+"일", 1+i), i);
+					}
+					</script>
+
+					<select name="year" id="years" style = "width: 80px; height: 40px"> </select>
+					<select name="month" id="months" style = "width: 80px; height: 40px"> </select>
+					<select name="day" id="days" style = "width: 80px; height: 40px"> </select>
 				</td>
 			</tr>
 
 			<tr>
 				<td colspan=2>
 					<input type=submit value="가입하기" style = "width: 100px; height: 40px">
+				</td>
+			</tr>
+
+			<tr>
+				<td colspan=2>
 				</td>
 			</tr>
 		</table>
--- membersave.php
+++ membersave.php
@@ -4,25 +4,40 @@
 
 	$id=$_GET['newid'];
 	$password=$_GET['newpwd'];
+	$mail=$_GET['mail'];
+	$phonenum=$_GET['phonenum'];
+	$year=$_GET['year'];
+	$month=$_GET['month'];
+	$day=$_GET['day'];
 
-	$sql = "insert into user (id, password) values ('$id', '$password')";
+	if($month<10)
+		$month="0".$month;
 
+	if($day<10)
+		$day="0".$day;
+
+	$total="$year$month$day";
+
+	$sql = "insert into user (id, password, e_mail, phone_num, birthday) values ('$id', '$password', '$mail', '$phonenum', '$total' )";
 
 	$check = "select * from user where id='$id'";
     $result = mysql_query($check);
     $row = mysql_fetch_row($result);
 
-	if(strcmp($id, $row[0]) == 0){
-		echo "<script>alert('동일한 아이디가 존재합니다.');</script>";
-		echo "<script>history.go(-1);</script>";
+	if(empty($id) || empty($password)){
+		if(empty($id) && empty($password))
+			echo "<script>alert('아이디/비밀번호가 입력되지 않았습니다.'); history.go(-1);</script>";
+		else if(empty($id))
+			echo "<script>alert('아이디가 입력되지 않았습니다.'); history.go(-1);</script>";
+		else
+			echo "<script>alert('비밀번호가 입력되지 않았습니다.'); history.go(-1);</script>";
 	}
-	else{
-		if((isset($id) && isset($password))){
+
+	else if(strcmp($id, $row[0]) == 0)
+		echo "<script>alert('동일한 아이디가 존재합니다.'); history.go(-1);</script>";
+
+	else if(isset($id) && isset($password)){
 			mysql_query($sql);
 			echo "<script>alert('입력되었습니다.');window.location.replace('login.php');</script>";
-		}
-		else{
-			echo "<script>alert('오류!');</script>";
-		}
 	}
 ?>
