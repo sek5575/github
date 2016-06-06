276469c946db310f4b299206ef48b04e501a9047
sek5575 <eungyusong@gmail.com>
2016-06-06 20:38:12 +0900

    디자인 수정

--- login.php
+++ login.php
@@ -2,41 +2,43 @@
 <html>
 <head>
 	<style type = "text/css">
+		#edge {border-top-left-radius:8px; border-top-right-radius:8px; border-bottom-left-radius:8px; border-bottom-right-radius: 8px;}
+		#main {margin : 0 auto; weight : 100%; background-color : #5ac7db; height : 40px; margin-bottom : 30px; padding : 20px 0px;}
 	</style>
 </head>
-<body>
+<body style = "background-color : #f0f0f0;">
 <table style = "width: 1100px; height: 800px; margin: 0 auto">
 	<tr>
 		<td style = "width: 800px; height: 100px">
 		<div style = "text-align: center">
-		<img src = "loginimg/sns.jpg" height="120" width="200" alt="love">
+		<img src = "loginimg/sns.png" height="120" width="200" alt="love">
 		</div>
 		</td>
 
 		<td style = "width: 300px; height: 100px">
 		<form action="login_proc.php" method="GET">
-		<table style = "margin: 0 auto">
+		<table cellspacing="10" style = "margin: 0 auto">
 			<tr>
 				<td>
-					<P>아이디</P>
+					<P style = "font-size:20;">아이디</P>
 				</td>
 				<td>
-					<input type = 'text' name='username' pattern = ".{4,15}" title = "4~15자로 입력해주세요." />
+					<input type = 'text' id='edge' name='username' pattern = ".{4,15}" title = "4~15자로 입력해주세요." style = "width: 180px; height: 40px" />
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<p>비밀번호</p>
+					<p style = "font-size:20;">비밀번호</p>
 				</td>
 				<td>
-					<input type = 'password' name='password' pattern = ".{4,15}" title = "4~15자로 입력해주세요." />
+					<input type = 'password'  id='edge' name='password' pattern = ".{4,15}" title = "4~15자로 입력해주세요." style = "width: 180px; height: 40px" />
 				</td>
 			</tr>
 			<tr>
 				<td>
 				</td>
 				<td>
-				<input type ='submit' value = '로그인하기'/>
+				<input type ='submit' value = '로그인하기' style = "width: 80px; height: 30px" />
 				</td>
 			</tr>
 		</table>
@@ -45,14 +47,14 @@
 	</tr>
 	<tr>
 		<td>
-			<img src="loginimg/image1.png" id=image alt="YsjImage" style="width:650px; height:550px;">
+			<img src="loginimg/im1.jpg" id=image alt="YsjImage" style="width:650px; height:550px;">
 			<script>
 			var myImage=document.getElementById("image");
-			var imageArray=["loginimg/image1.png","loginimg/image2.jpg","loginimg/image3.jpg","loginimg/image4.jpg"];  //이미지배열을 만들어 넣어준다.
+			var imageArray=["loginimg/im1.jpg","loginimg/im2.jpg","loginimg/im3.jpg","loginimg/im4.jpg"];  //이미지배열을 만들어 넣어준다.
 			var imageIndex=0;
 
 			function changeImage(){
-				myImage.setAttribute("src",imageArray[imageIndex]); //myImage의 src속성 값을 바꿔준다.
+				myImage.setAttribute("src",imageArray[imageIndex]); //myImage의 src속성 값을 바꿔준다. 
 				imageIndex++;
 				if(imageIndex>=imageArray.length){ //이미지index값이 길이보다 커지면 index값을 0으로 바꿔준다
 					imageIndex=0;
@@ -64,25 +66,25 @@
 
 		<td>
 		<form name="join" method="GET" action="membersave.php"> 
-		<table style = "margin: 0 auto">
+		<table cellspacing="20" style = "margin: 0 auto">
 			<tr>
 				<td>
-					<input type="text" name="newid" placeholder = "아이디" style = "width: 150px; height: 40px"  pattern = ".{4,15}" title = "4~15자로 입력해주세요." >
+					<input type="text" id='edge' name="newid" placeholder = "아이디" style = "width: 150px; height: 40px; font-size:18;"  pattern = ".{4,15}" title = "4~15자로 입력해주세요." >
 				</td>
 				<td>
-					<input type="password" name="newpwd" placeholder = "비밀번호" style = "width: 150px; height: 40px" pattern = ".{4,15}" title = "4~15자로 입력해주세요." >
+					<input type="password" id='edge' name="newpwd" placeholder = "비밀번호" style = "width: 150px; height: 40px;  font-size:18;" pattern = ".{4,15}" title = "4~15자로 입력해주세요.">
 				</td>
 			</tr>
 
 			<tr>
 				<td colspan=2>
-					<input type="text" name="mail" pattern = ".{1,15}@.{1,15}\..{1,15}" title = "e-mail 주소를 정확하게 넣어주세요." placeholder = "e-mail" style = "width: 300px; height: 40px">
+					<input type="text" id='edge' name="mail" pattern = ".{1,15}@.{1,15}\..{1,5}" title = "e-mail 주소를 정확하게 넣어주세요." placeholder = "e-mail" style = "width: 320px; height: 40px;  font-size:18;">
 				</td>
 			</tr>
 
 			<tr>
 				<td colspan=2>
-					<input type="text" name="phonenum" pattern = "[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}" title = "번호를 정확하게 넣어주세요." placeholder = "000-0000-0000" style = "width: 300px; height: 40px ">
+					<input type="text" id='edge' name="phonenum" pattern = "[0-9]{3}-[0-9]{3,4}-[0-9]{4}" title = "번호를 정확하게 넣어주세요." placeholder = "000-0000-0000" style = "width: 320px; height: 40px; font-size:18;">
 				</td>
 			</tr>
 
@@ -122,15 +124,22 @@
 					<input type=submit value="가입하기" style = "width: 100px; height: 40px">
 				</td>
 			</tr>
-
-			<tr>
-				<td colspan=2>
-				</td>
-			</tr>
 		</table>
 		</form>
 		</td>
 	</tr>
+
+	<tr>
+		<td>
+			<h2>여러분이 하고 싶은일을 적어보세요</h2>
+			<h3>지금 하고 있는 일들을 친구들과 공유해요</h3>
+			<p>KSL © 2016</p>
+		</td>
+		<td>
+
+		</td>
+	</tr>
+
 </table>
 </body>
 
